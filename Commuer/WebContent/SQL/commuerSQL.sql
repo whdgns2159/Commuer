708cf106e7f0c836bf5f920b0dbf5c7a4092007d
1class-15 <1class-15@1class-15-PC>
2019-01-10 17:51:46 +0900

    01/10

--- Commuer/WebContent/SQL/commuerSQL.sql
+++ Commuer/WebContent/SQL/commuerSQL.sql
@@ -90,9 +90,99 @@ create table humor_board_reply(
 
 drop table humor_board_reply;
 --------------------------------------------------------------------------------------------
+--음악게시판 인덱스 시퀀스--
+create SEQUENCE music_board_seq 
+increment by 1 START WITH 1 nocache;
+
+drop sequence music_board_seq;
+--음악게시판 공감수 시퀀스--
+create sequence music_likes_seq
+increment by 1 start with 1 nocache;
+
+drop sequence music_likes_seq;
+--음악게시판 테이블 생성--
+create table music_board(
+  music_b_num number not null, --글번호
+  subject varchar2(250) not null, --제목
+  content long not null, --글내용
+  id varchar2(20) not null, --작성자
+  hits number(6) default 0, --조회수
+  likes number(6) default 0, --공감수
+  wdate date, --작성일
+  constraint music_b_num_pk primary key (music_b_num), --기본키 (글번호)
+  constraint music_b_fk foreign key (id)  references user_Info(id) -- 참조키 (id) 
+);
+
+drop table music_board;
+
+--음악게시판 댓글 인덱스 시퀀스--
+create sequence music_board_reply_seq
+increment by 1 start with 1 nocache;
+
+drop sequence music_board_reply_seq;
+
+--음악게시판 댓글테이블 생성--
+create table music_board_reply(
+  music_b_r_num number not null,--댓글번호
+  music_b_num number not null, --참조할 게시글번호
+  id varchar2(20) not null, --댓글작성자
+  content long not null,--댓글내용
+  wdate date, --작성시간
+  constraint music_b_r_num_pk primary key (music_b_r_num),
+  constraint music_b_num_fk foreign key (music_b_num) 
+  references music_board(music_b_num)
+);
+
+drop table music_board_reply;
+
+--------------------------------------------------------------------------------------------
+--연예게시판 인덱스 시퀀스--
+create SEQUENCE star_board_seq 
+increment by 1 START WITH 1 nocache;
+
+drop sequence star_board_seq;
+--연예게시판 공감수 시퀀스--
+create sequence star_likes_seq
+increment by 1 start with 1 nocache;
+
+drop sequence star_likes_seq;
+--연예게시판 테이블 생성--
+create table star_board(
+  star_b_num number not null, --글번호
+  subject varchar2(250) not null, --제목
+  content long not null, --글내용
+  id varchar2(20) not null, --작성자
+  hits number(6) default 0, --조회수
+  likes number(6) default 0, --공감수
+  wdate date, --작성일
+  constraint star_b_num_pk primary key (star_b_num), --기본키 (글번호)
+  constraint star_b_fk foreign key (id)  references user_Info(id) -- 참조키 (id) 
+);
+
+drop table star_board;
+
+--연예게시판 댓글 인덱스 시퀀스--
+create sequence star_board_reply_seq
+increment by 1 start with 1 nocache;
+
+drop sequence star_board_reply_seq;
+
+--연예게시판 댓글테이블 생성--
+create table star_board_reply(
+  star_b_r_num number not null,--댓글번호
+  star_b_num number not null, --참조할 게시글번호
+  id varchar2(20) not null, --댓글작성자
+  content long not null,--댓글내용
+  wdate date, --작성시간
+  constraint star_b_r_num_pk primary key (star_b_r_num),
+  constraint star_b_num_fk foreign key (star_b_num) 
+  references star_board(star_b_num)
+);
+
+drop table star_board_reply;
 
 
-
+--------------------------------------------------------------------------------------------
 --유저정보---------------------------------------------------------------------------------
 --유저인덱스
 create sequence user_seq
